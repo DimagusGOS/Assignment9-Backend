a015ae4171b51581c59b8d5c01120c944b2e145e
Diego <diegomano2@hotmail.com>
2025-06-16 17:49:25 -0700

    task 6 completed, tested with postman

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config();
+mongoose
+    .connect(process.env.MONGODB_URI)
+    .then(() => console.log("Connected to MongoDB Atlas"))
+    .catch((err) =>
+        console.error("MongoDB connection error:", err));
 
 const express = require('express');
 const cors = require('cors');
